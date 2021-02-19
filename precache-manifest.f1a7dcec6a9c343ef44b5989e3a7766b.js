00f9a64eee18ba4f3eda0cd2255ebd4882658496
Graeme <graemster@gmail.com>
2021-02-19 03:38:44 -0600

    Updates

---
+++ precache-manifest.f1a7dcec6a9c343ef44b5989e3a7766b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4aaccca54cdde38c510edb3f9884e6",
+    "url": "/dad-jokes-generator/index.html"
+  },
+  {
+    "revision": "061c90ef79b8be5624be",
+    "url": "/dad-jokes-generator/static/css/main.ee3cca6e.chunk.css"
+  },
+  {
+    "revision": "b4dc49cf062696718f26",
+    "url": "/dad-jokes-generator/static/js/2.9b720001.chunk.js"
+  },
+  {
+    "revision": "061c90ef79b8be5624be",
+    "url": "/dad-jokes-generator/static/js/main.57f560b8.chunk.js"
+  },
+  {
+    "revision": "ecd28d3f7133e534c577",
+    "url": "/dad-jokes-generator/static/js/runtime~main.9f83b11b.js"
+  }
+]);
